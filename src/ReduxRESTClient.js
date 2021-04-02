066f0dea36519937d81b4a33745b4c6db434d43a
Wesley Unwin <wes_unwin@hotmail.com>
2021-04-01 23:23:39 -0400

    README: fix options.path usage

--- src/ReduxRESTClient.js
+++ src/ReduxRESTClient.js
@@ -23,7 +23,7 @@ class ReduxRESTClient {
    * 
    *  class ChatMessages extends RecordSet {
    *    constructor() {
-   *      super('chatMessages', { path: 'chat_messages', createSlice: createSlice, createSelector: createSelector });
+   *      super('chatMessages', { path: '/chat_messages', createSlice: createSlice, createSelector: createSelector });
    *    }
    *  }
    * 
